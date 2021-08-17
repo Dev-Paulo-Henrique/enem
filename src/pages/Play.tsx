991f0f58a9f92140064c5fe7e8d0235e11061d48
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-08-17 16:09:30 -0300

    Test

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -20,27 +20,31 @@ export function Play() {
         </div>
       </header>
       <main>
-        <p>questao</p>
+        <p>Brasil, Alemanha, Japão e Índia pedem reforma do Conselho de Segurança
+
+Os representantes do G4 (Brasil, Alemanha, Índia e Japão) reiteraram, em setembro  de 2018, a defesa pela ampliação do Conselho de Segurança da Organização das  Nações Unidas (ONU) durante reunião em Nova York (Estados Unidos). Em declaração conjunta, de dez itens, os chanceleres destacaram que o órgão, no formato em que está, com apenas cinco membros permanentes e dez rotativos, não reflete o século 21.   “A reforma do Conselho de Segurança é essencial para enfrentar os desafios complexos de hoje. Como aspirantes a novos membros permanentes de um conselho reformado, os ministros reiteraram seu compromisso de trabalhar para fortalecer o funcionamento da ONU e da ordem multilateral global, bem como seu apoio às respectivas candidaturas”, afirma a declaração conjunta.
+
+Os países mencionados no texto justificam sua pretensão com base na seguinte característica comum:</p>
         <div className="alt">
         <div className="a">
         <input type="checkbox" name="a" id="a" />
-        <label htmlFor="">oi</label>
+        <label htmlFor="">Extensividade de área territorial.</label>
         </div>
         <div className="b">
         <input type="checkbox" name="b" id="b" />
-        <label htmlFor="">oi</label>
+        <label htmlFor="">Protagonismo em escala regional.</label>
         </div>
         <div className="c">
         <input type="checkbox" name="c" id="c" />
-        <label htmlFor="">oi</label>
+        <label htmlFor="">Investimento em tecnologia militar.</label>
         </div>
         <div className="d">
         <input type="checkbox" name="d" id="d" />
-        <label htmlFor="">oi</label>
+        <label htmlFor=""> Desenvolvimento de energia nuclear.</label>
         </div>
         <div className="e">
         <input type="checkbox" name="e" id="e" />
-        <label htmlFor="">oi</label>
+        <label htmlFor="">Disponibilidade de recursos minerais.</label>
         </div>
         
         </div>
