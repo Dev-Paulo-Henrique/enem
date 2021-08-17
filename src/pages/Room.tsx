57a5544f038aab17c0f32a2d4a309249ab39649b
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-08-17 18:35:09 -0300

    Test

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -47,31 +47,31 @@ export function Room() {
         <div className="together">
         <div className="content">
         <div className="background one"></div>
-            <a href="https://">
+            <a href="/matter/ciencias-da-natureza">
               Ciências da Natureza
               </a>
               </div>
               <div className="content">
               <div className="background two"></div>
-            <a href="https://">
+            <a href="/matter/ciencias-da-natureza">
               Ciências Humanas
               </a>
               </div>
               <div className="content">
               <div className="background three"></div>
-            <a href="https://">
+            <a href="/matter/liguagens-codigos-e-suas-tecnologias">
               Liguagens, Códigos e suas Tecnologias
               </a>
               </div>
               <div className="content">
               <div className="background four"></div>
-            <a href="https://">
+            <a href="/matter/matematica-e-suas-tecnologias">
               Matemática e suas tecnologias
               </a>
               </div>
               <div className="content">
               <div className="background five"></div>
-            <a href="https://">
+            <a href="/matter/redacao">
               Redação
               </a>
               </div>
