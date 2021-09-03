9fcbf08db30a3e84aca318c280b23c11c53faf90
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-09-03 14:59:08 -0300

    Test

--- src/pages/Favoritos.tsx
+++ src/pages/Favoritos.tsx
@@ -6,6 +6,7 @@ import { useHistory} from 'react-router-dom'
 import { auth, database } from '../services/firebase'
 import { useEffect, useState } from 'react';
 import { Question } from '../components/Question';
+import { List } from '../components/List';
 
 type FirebaseQuestions = Record<string, {
   type: string;
@@ -93,41 +94,7 @@ export function Fav() {
         </div>
       </header>
       <div className="matter">
-        <nav>
-        <div className="together">
-        <h1>Meus envios</h1>
-        <div className="content">
-        <div className="background one"></div>
-            <a href="/matter/ciencias-da-natureza">
-              Ciências da Natureza
-              </a>
-              </div>
-              <div className="content">
-              <div className="background three"></div>
-            <a href="/matter/liguagens-codigos-e-suas-tecnologias">
-              Liguagens, Códigos e suas Tecnologias
-              </a>
-              </div>
-              <div className="content">
-              <div className="background two"></div>
-            <a href="/matter/ciencias-humana">
-              Ciências Humanas
-              </a>
-              </div>
-              <div className="content">
-              <div className="background four"></div>
-            <a href="/matter/matematica-e-suas-tecnologias">
-              Matemática e suas tecnologias
-              </a>
-              </div>
-              <div className="content">
-              <div className="background five"></div>
-            <a href="/matter/redacao">
-              Redação
-              </a>
-              </div>
-              </div>
-        </nav>
+        <List/>
         <div className="xp">
         <aside>
         <div className="background"></div>
