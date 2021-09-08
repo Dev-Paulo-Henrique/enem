b0bc02989c267e506378d6e74175cff18320e260
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-09-08 16:59:01 -0300

    Test

--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -1,79 +1,40 @@
-import { useEffect, useState } from "react"
-import { Question } from "../Question"
+import { ReactNode } from "react"
 import { useAuth } from "../../hooks/useAuth"
-import { database } from "../../services/firebase"
 
-type FirebaseQuestions = Record<string, {
-  Id: string;
-  Photo: string;
-  type: string;
-  title: string;
-  authorId: string;
-  createdAt: string;
+type UserProps = {
   content: string;
-  author: string;
-}>
-
-type UsersType = {
+  title: string;
+  type: string;
   id: string;
-  title: string;
-  content: string;
-  authorId: string;
-  type: string;
-  createdAt: string;
   author: string;
+  authorId: string;
+  createdAt: string;
+  children?: ReactNode;
 }
 
-export function User() {
+export function User({
+  content,
+  title,
+  type,
+  id,
+  author,
+  authorId,
+  createdAt,
+  children,
+}: UserProps) {
   const { user } = useAuth()
-  const [users, setUsers] = useState<UsersType[]>([])
 
-  useEffect(() => {
-    const roomRef = database.ref(`all`)//criar outra camada
-    //console.log(roomRef.key)
-    roomRef.on('value', room => {
-      const databaseRoom = room.val()
-      const firebaseQuestions: FirebaseQuestions = databaseRoom ?? {}
-
-
-      const parsedQuestion = Object.entries(firebaseQuestions).map(([key, value]) => {
-        return {
-          id: key,
-          title: value.title,
-          content: value.content,
-          type: value.type,
-          author: value.author,
-          createdAt: value.createdAt,
-          authorId: value.authorId,
-        }
-      })
-      //console.log(parsedQuestion)
-      setUsers(parsedQuestion)
-      //console.log(databaseRoom.admin)
-      //return console.log(JSON.stringify({databaseRoom}))
-    })
-
-    return () => {
-      roomRef.off('value')
-      //console.log(roomRef)
-    }
-  }, [user?.name])
   return (
     <>
-    {users.map(user => {
-      return (
-        <Question
-          key={user.id}
-          content={user.content}
-          title={user.title}
-          type={user.type}
-          id={user.id}
-          author={user.author}
-          createdAt={user.createdAt}
-          authorId={user.authorId}
-        />
-      )
-    })}
+    <div className="profile">oi {user?.name}</div>
+    <p>{content}</p>
+    <p>{title}</p>
+    <p>{type}</p>
+    <p>{id}</p>
+    <p>{author}</p>
+    <p>{authorId}</p>
+    <p>{createdAt}</p>
+    <p>{children}</p>
     </>
   )
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,80 @@
+import { useEffect, useState } from 'react'
+import { User } from '../components/users/index'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+type FirebaseQuestions = Record<string, {
+  Id: string;
+  Photo: string;
+  type: string;
+  title: string;
+  createdAt: string;
+  content: string;
+  author: string;
+  authorId: string;
+}>
+
+type UsersType = {
+  id: string;
+  title: string;
+  content: string;
+  type: string;
+  createdAt: string;
+  author: string;
+  authorId: string;
+}
+
 export function Profile(){
+  const { user } = useAuth()
+  const [users, setUsers] = useState<UsersType[]>([])
+
+  useEffect(() => {
+    const roomRef = database.ref(`all`)//criar outra camada
+    //console.log(roomRef.key)
+    roomRef.on('value', room => {
+      const databaseRoom = room.val()
+      const firebaseQuestions: FirebaseQuestions = databaseRoom ?? {}
+
+
+      const parsedQuestion = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          title: value.title,
+          content: value.content,
+          type: value.type,
+          author: value.author,
+          createdAt: value.createdAt,
+          authorId: value.authorId,
+        }
+      })
+      //console.log(parsedQuestion)
+      setUsers(parsedQuestion)
+      //console.log(databaseRoom.admin)
+      //return console.log(JSON.stringify({databaseRoom}))
+    })
+
+    return () => {
+      roomRef.off('value')
+      //console.log(roomRef)
+    }
+  }, [user?.name])
+
   return(
-    <h1>oi</h1>
+    <>
+    {users.map(user => {
+      return (
+        <User
+          key={user.id}
+          content={user.content}
+          title={user.title}
+          type={user.type}
+          id={user.id}
+          author={user.author}
+          createdAt={user.createdAt}
+          authorId={user.authorId}
+        />
+      )
+    })}
+    </>
   )
 }
